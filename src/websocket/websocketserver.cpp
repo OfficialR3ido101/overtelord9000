3ce3a7db653e19c2cae6d86b7c84096198bfaf69
OfficialR3ido101 <oliver.reid9090@gmail.com>
2023-06-29 17:03:24 +0100

    fixed formatting

--- src/websocket/websocketserver.cpp
+++ src/websocket/websocketserver.cpp
@@ -2,29 +2,29 @@
 #include <qlogging.h>
 #include <QDebug>
 
-    void BotWsServer::run() {
+void BotWsServer::run() {
 
-        m_endpoint.listen(std::stoi(getenv("WEBSOCKET_PORT")));
+    m_endpoint.listen(std::stoi(getenv("WEBSOCKET_PORT")));
 
-        // Queues a connection accept operation
-        m_endpoint.start_accept();
+    // Queues a connection accept operation
+    m_endpoint.start_accept();
 
-        qDebug() << "[Websocket]" << "Websocket server starting!";
+    qDebug() << "[Websocket]" << "Websocket server starting!";
 
-        // Start the Asio io_service run loop
-        m_endpoint.run();
-    }
+    // Start the Asio io_service run loop
+    m_endpoint.run();
+}
 
-    void BotWsServer::sendClientMessage (std::string message) {
-        sendMessage(message);
-    }
+void BotWsServer::sendClientMessage (std::string message) {
+    sendMessage(message);
+}
 
-    void BotWsServer::on_open(websocketpp::connection_hdl hdl) {
-        m_connections.insert(hdl);
-    }
+void BotWsServer::on_open(websocketpp::connection_hdl hdl) {
+    m_connections.insert(hdl);
+}
 
-    void BotWsServer::on_close(websocketpp::connection_hdl hdl) {
-        m_connections.erase(hdl);
-        qInfo() << "[Websocket] " << "Closed Websocket server!";
-        emit finished();
-    }
+void BotWsServer::on_close(websocketpp::connection_hdl hdl) {
+    m_connections.erase(hdl);
+    qInfo() << "[Websocket] " << "Closed Websocket server!";
+    emit finished();
+}
